47b1ced7a3eac3462b6154e80fac479da6b1ab03
hartaithan <hartaithan@gmail.com>
2022-03-17 15:10:17 +0900

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Display from "./components/Display/Display";
+import Keyboard from "./components/Keyboard/Keyboard";
 
-const App = () => {
-  return <div className="App">Hello world!</div>;
+const App: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <div className="body">
+        <Display />
+        <Keyboard />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Keyboard/Keyboard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../Button/Button";
+
+const Keyboard: React.FC = () => {
+  return (
+    <div className="keyboard">
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+};
+
+export default Keyboard;
